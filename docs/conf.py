2716ae180cb3a3f14678bac5f27e4d1514feb965
Tudor Aursulesei <thestick613@gmail.com>
2017-02-23 12:52:56 +0200

    fix version

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = u'ThunderCache'
 year = '2017'
 author = u'Tudor Aursulesei'
 copyright = '{0}, {1}'.format(year, author)
-version = release = u'0.1.0'
+version = release = u'0.1.1'
 
 pygments_style = 'trac'
 templates_path = ['.']
